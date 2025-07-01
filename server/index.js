a9bf55b314957b65c92657455c1b839eb0dca202
rmohitzz21 <rmohit21264@gmail.com>
2025-07-01 15:37:15 +0530

    Mongose and server is Running..

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+dotenv.config()
+import morgan from 'morgan';
+import helmet from 'helmet'
+
+import connectDB from './config/connectDB.js';
+const app = express();
+
+app.use(cors({
+    credentials:true,
+    origin: process.env.FRONTED_URL
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginEmbedderPolicy: false
+}))
+
+const PORT = 8080 || process.env.PORT
+
+app.get("/", (req,res)=> {
+    // server to client 
+    res.json({
+        message: "Server is Running"
+    })
+})
+
+connectDB().then(() =>{
+    app.listen(PORT, () => {
+        console.log("Server is Running ",PORT);
+    })
+})
+
